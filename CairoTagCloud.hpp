fbbd539751ac0df66c5e2f8f8645654c7c577780
Erik Saule <esaule@uncc.edu>
2013-10-23 17:13:10 -0400

    first tagcloud

--- CairoTagCloud.hpp
+++ CairoTagCloud.hpp
@@ -7,27 +7,104 @@ class CairoTagCloud : public CairoGraphicController {
   
   std::vector<std::pair<std::string, int > > count;
   
-  float fontsize (int occurence) {return occurence;}
+  //  float fontsize (int occurence) {return occurence;}
+  float fontsize (int occurence) {return 3*sqrt((float)occurence)+5;}
 
   struct placement {
     float x, y, w, h;
+
+    std::string tostring() const {
+      std::stringstream ss;
+      ss<<"("<<x<<", "<<y<<", "<<w<<", "<<h<<")";
+      return ss.str();
+    }
   };
 
   std::map<std::string, placement > actual_placement;
+
+  //return true if collide
+  bool check_collision(const placement& p1, const placement& p2) {
+    float bl = p2.x;
+    float br = p2.x+p2.w;
+    float bt = p2.y+p2.h;
+    float bb = p2.y;
+
+    float al = p1.x;
+    float ar = p1.x+p1.w;
+    float at = p1.y+p1.h;
+    float ab = p1.y;    
+    
+    if (bl >= ar) return false;
+    if (br <= al) return false;
+    if (bb >= at) return false;
+    if (bt <= ab) return false;
+
+    return true;
+  }
   
   void compute_placement() {
+    if (!refresh_placement) return;
+
+    std::cout<<"compute placement"<<std::endl;
+    
+    refresh_placement = false;
+
     actual_placement.clear();
     for (auto ent : count) {
-      
+      placement p;
+
+      bool cont = true;
+
+      int try_count = 0;
+
+      while (cont) {
+	try_count ++;
+	if (try_count == 50) break;
+	cont = false;
+	//compute tentative placement
+	p.h = fontsize(ent.second);
+	p.w = fontsize(ent.second)*ent.first.size();
+	p.x = rand() % (getSizeX()-(int) p.w);
+	p.y = p.h+ (rand() % ( getSizeY() - (int) p.y));
+
+	//check for collisions
+	for (auto ent : actual_placement) {
+	  placement& pl = ent.second;
+	  if (check_collision(p, pl))
+	    cont = true;
+	  if (cont) {
+	    std::cout<<"collide"<<p.tostring()<<" "<<pl.tostring()<<std::endl;
+	    break;
+	  }
+	}
+      } 
+      if (try_count < 50)
+	actual_placement[ent.first] = p;
     }
   }
+
+  bool refresh_placement;
+  cairo_pattern_t * bgcolor;
+  cairo_pattern_t * fgcolor;
 
 public:
   
   CairoTagCloud (  std::vector<std::pair<std::string, int > >& c )
     :count(c) {
-    compute_placement();
+    refresh_placement = true;
+
+    bgcolor = cairo_pattern_create_rgb(1,1,1);
+    fgcolor = cairo_pattern_create_rgb(0,0,0);
   }
+
+  virtual ~CairoTagCloud() {   
+    cairo_pattern_destroy(bgcolor);
+    cairo_pattern_destroy(fgcolor);
+  }
+
+  virtual void setSizeX(int sx){if (sx != getSizeX()) refresh_placement = true; CairoGraphicController::setSizeX(sx);}
+  virtual void setSizeY(int sy){if (sy != getSizeY()) refresh_placement = true; CairoGraphicController::setSizeY(sy); }
+
   
   virtual void clickat(int x, int y){}
   
@@ -35,7 +112,26 @@ public:
 
   virtual void clickrelease(int x, int y){}
 
-  virtual void render(cairo_t* cr){}
+  virtual void render(cairo_t* cr) {
+    compute_placement();
+
+    //paint background
+    cairo_set_source(cr, bgcolor);
+    cairo_paint(cr);
+
+
+    //paint rectangles
+    cairo_set_source(cr, fgcolor);
+    for (auto pl : actual_placement) {
+      
+      //cairo_rectangle(cr, pl.second.x, pl.second.y, pl.second.w, pl.second.h);
+      //cairo_stroke(cr);
+
+      cairo_move_to (cr, pl.second.x, pl.second.y + pl.second.h);
+
+      show_text (cr, pl.first, pl.second.h);
+    }
+  }
 
   virtual bool quit() const{return false;}
 
--- Makefile
+++ Makefile
@@ -1,7 +1,34 @@
 LD=g++
 
-BIBTEXSPIRIT_INC= -I $(HOME)/prgs/bibtex-spirit/include
-CXXFLAGS= $(BIBTEXSPIRIT_INC) -std=c++11
+BIBTEXSPIRIT_INC = -I $(HOME)/prgs/bibtex-spirit/include
+CXXFLAGS = $(BIBTEXSPIRIT_INC)
 
-main: main.o parse.o
-	$(LD) -o main main.o parse.o
+CXXFLAGS += -std=c++11
+
+
+GTK_INCLUDE = `pkg-config --cflags gtk+-2.0` `pkg-config --cflags cairo`
+GTK_LIB = `pkg-config --libs gtk+-2.0``pkg-config --libs cairo`
+
+CXXFLAGS += $(GTK_INCLUDE)
+
+LDFLAGS += GTK_LIB
+
+OBJS = main.o parse.o log.o
+
+main: $(OBJS)
+	$(LD) $(GTK_LIB) -o main $(OBJS)
+
+depend: 
+	makedepend -f Makefile *.cpp *.hpp -Y util/*.cpp util/*.hpp
+# DO NOT DELETE
+
+log.o: log.hpp
+main.o: parse.hpp util.hpp CairoTagCloud.hpp cairo_graphic_controller.hpp
+main.o: serializable.hpp exception.hpp util/stacktrace.hpp log.hpp
+parse.o: util.hpp
+CairoTagCloud.o: cairo_graphic_controller.hpp serializable.hpp exception.hpp
+CairoTagCloud.o: util/stacktrace.hpp log.hpp
+cairo_graphic_controller.o: serializable.hpp exception.hpp
+cairo_graphic_controller.o: util/stacktrace.hpp log.hpp
+exception.o: util/stacktrace.hpp log.hpp
+serializable.o: exception.hpp util/stacktrace.hpp log.hpp
